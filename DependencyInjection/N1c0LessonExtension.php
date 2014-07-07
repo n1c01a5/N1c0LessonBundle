e857642b8ccd2c009bd2cf28abdf9a2849b24e01
n1c01a5 <contact@wagner-nicolas.com>
2014-07-07 13:59:04 +0200

    Remove Introdution element

--- DependencyInjection/N1c0LessonExtension.php
+++ DependencyInjection/N1c0LessonExtension.php
@@ -40,35 +40,25 @@ class N1c0LessonExtension extends Extension
         }
 
         $container->setParameter('n1c0_lesson.model.lesson.class', $config['class']['model']['lesson']);
-        $container->setParameter('n1c0_lesson.model.introduction.class', $config['class']['model']['introduction']);
         $container->setParameter('n1c0_lesson.model.chapter.class', $config['class']['model']['chapter']);
-        $container->setParameter('n1c0_lesson.model.argument.class', $config['class']['model']['argument']);
         $container->setParameter('n1c0_lesson.model.conclusion.class', $config['class']['model']['conclusion']);
 
         $container->setParameter('n1c0_lesson.model_manager_name', $config['model_manager_name']);
 
         $container->setParameter('n1c0_lesson.form.lesson.type', $config['form']['lesson']['type']);
-        $container->setParameter('n1c0_lesson.form.introduction.type', $config['form']['introduction']['type']);
         $container->setParameter('n1c0_lesson.form.chapter.type', $config['form']['chapter']['type']);
-        $container->setParameter('n1c0_lesson.form.argument.type', $config['form']['argument']['type']);
         $container->setParameter('n1c0_lesson.form.conclusion.type', $config['form']['conclusion']['type']);
 
         $container->setParameter('n1c0_lesson.form.lesson.name', $config['form']['lesson']['name']);
-        $container->setParameter('n1c0_lesson.form.introduction.name', $config['form']['introduction']['name']);
         $container->setParameter('n1c0_lesson.form.chapter.name', $config['form']['chapter']['name']);
-        $container->setParameter('n1c0_lesson.form.argument.name', $config['form']['argument']['name']);
         $container->setParameter('n1c0_lesson.form.conclusion.name', $config['form']['conclusion']['name']);
 
         $container->setAlias('n1c0_lesson.form_factory.lesson', $config['service']['form_factory']['lesson']);
-        $container->setAlias('n1c0_lesson.form_factory.introduction', $config['service']['form_factory']['introduction']);
         $container->setAlias('n1c0_lesson.form_factory.chapter', $config['service']['form_factory']['chapter']);
-        $container->setAlias('n1c0_lesson.form_factory.argument', $config['service']['form_factory']['argument']);
         $container->setAlias('n1c0_lesson.form_factory.conclusion', $config['service']['form_factory']['conclusion']);
 
         $container->setAlias('n1c0_lesson.manager.lesson', $config['service']['manager']['lesson']);
-        $container->setAlias('n1c0_lesson.manager.introduction', $config['service']['manager']['introduction']);
         $container->setAlias('n1c0_lesson.manager.chapter', $config['service']['manager']['chapter']);
-        $container->setAlias('n1c0_lesson.manager.argument', $config['service']['manager']['argument']);
         $container->setAlias('n1c0_lesson.manager.conclusion', $config['service']['manager']['conclusion']);
 
         // Add a condition if markup so...
@@ -82,16 +72,12 @@ class N1c0LessonExtension extends Extension
 
         foreach (array(1 => 'create', 'view', 'edit', 'delete') as $index => $perm) {
             $container->getDefinition('n1c0_lesson.acl.lesson.roles')->replaceArgument($index, $config['acl_roles']['lesson'][$perm]);
-            $container->getDefinition('n1c0_lesson.acl.introduction.roles')->replaceArgument($index, $config['acl_roles']['introduction'][$perm]);
             $container->getDefinition('n1c0_lesson.acl.chapter.roles')->replaceArgument($index, $config['acl_roles']['chapter'][$perm]);
-            $container->getDefinition('n1c0_lesson.acl.argument.roles')->replaceArgument($index, $config['acl_roles']['argument'][$perm]);
             $container->getDefinition('n1c0_lesson.acl.conclusion.roles')->replaceArgument($index, $config['acl_roles']['conclusion'][$perm]);
         }
 
         $container->setAlias('n1c0_lesson.acl.lesson', $config['service']['acl']['lesson']);
-        $container->setAlias('n1c0_lesson.acl.introduction', $config['service']['acl']['introduction']);
         $container->setAlias('n1c0_lesson.acl.chapter', $config['service']['acl']['chapter']);
-        $container->setAlias('n1c0_lesson.acl.argument', $config['service']['acl']['argument']);
         $container->setAlias('n1c0_lesson.acl.conclusion', $config['service']['acl']['conclusion']);
     }
 }
--- Events.php
+++ Events.php
@@ -43,47 +43,6 @@ final class Events
      */
     const LESSON_CREATE = 'n1c0_lesson.lesson.create';
 
-
-    /**
-     * The PRE_PERSIST event occurs prior to the persistence backend
-     * persisting the Introduction.
-     *
-     * This event allows you to modify the data in the Introduction prior
-     * to persisting occuring. The listener receives a
-     * N1c0\LessonBundle\Event\IntroductionPersistEvent instance.
-     *
-     * Persisting of the introduction can be aborted by calling
-     * $event->abortPersist()
-     *
-     * @var string
-     */
-    const INTRODUCTION_PRE_PERSIST = 'n1c0_lesson.introduction.pre_persist';
-
-    /**
-     * The POST_PERSIST event occurs after the persistence backend
-     * persisted the Argument.
-     *
-     * This event allows you to notify users or perform other actions
-     * that might require the Introduction to be persisted before performing
-     * those actions. The listener receives a
-     * N1c0\LessonBundle\Event\IntroductionEvent instance.
-     *
-     * @var string
-     */
-    const INTRODUCTION_POST_PERSIST = 'n1c0_lesson.introduction.post_persist';
-
-    /**
-     * The CREATE event occurs when the manager is asked to create
-     * a new instance of a Introduction.
-     *
-     * The listener receives a N1c0\LessonBundle\Event\IntroductionEvent
-     * instance.
-     *
-     * @var string
-     */
-    const INTRODUCTION_CREATE = 'n1c0_lesson.introduction.create';
-
-    
     /**
      * The PRE_PERSIST event occurs prior to the persistence backend
      * persisting the Chapter.
@@ -122,45 +81,6 @@ final class Events
      * @var string
      */
     const CHAPTER_CREATE = 'n1c0_lesson.chapter.create';
-
-    /**
-     * The PRE_PERSIST event occurs prior to the persistence backend
-     * persisting the Argument.
-     *
-     * This event allows you to modify the data in the Argument prior
-     * to persisting occuring. The listener receives a
-     * N1c0\LessonBundle\Event\ArgumentPersistEvent instance.
-     *
-     * Persisting of the argument can be aborted by calling
-     * $event->abortPersist()
-     *
-     * @var string
-     */
-    const ARGUMENT_PRE_PERSIST = 'n1c0_lesson.argument.pre_persist';
-
-    /**
-     * The POST_PERSIST event occurs after the persistence backend
-     * persisted the Argument.
-     *
-     * This event allows you to notify users or perform other actions
-     * that might require the Argument to be persisted before performing
-     * those actions. The listener receives a
-     * N1c0\LessonBundle\Event\ArgumentEvent instance.
-     *
-     * @var string
-     */
-    const ARGUMENT_POST_PERSIST = 'n1c0_lesson.argument.post_persist';
-
-    /**
-     * The CREATE event occurs when the manager is asked to create
-     * a new instance of a Argument.
-     *
-     * The listener receives a N1c0\LessonBundle\Event\ArgumentEvent
-     * instance.
-     *
-     * @var string
-     */
-    const ARGUMENT_CREATE = 'n1c0_lesson.argument.create';
 
     /**
      * The PRE_PERSIST event occurs prior to the persistence backend
